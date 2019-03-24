89dd624b819eecf2014f55a0c3b66028bd47b815
Avaer Kazmer <a@modules.io>
2019-03-24 03:30:14 -0400

    Only extract on linux platform

--- postinstall.js
+++ postinstall.js
@@ -2,11 +2,21 @@ const stream = require('stream');
 const path = require('path');
 const fs = require('fs');
 const child_process = require('child_process');
+const os = require('os');
 const unbr = require('unbr');
 const rimraf = require('rimraf');
 
 const lib = path.join(__dirname, 'lib4.tar.br');
 const libFiles = ['lib4.tar.br01', 'lib4.tar.br02'].map(f => path.join(__dirname, f));
+const platform = (() => {
+  if (process.env['LUMIN'] !== undefined) {
+    return 'lumin';
+  } else if (process.env['ANDROID'] !== undefined) {
+    return 'android';
+  } else {
+    return os.platform();
+  }
+})();
 const _cleanup = () => {
   const files = libFiles.concat([lib]);
   for (let i = 0; i < files.length; i++) {
@@ -17,12 +27,10 @@ const _cleanup = () => {
     });
   }
 };
-unbr(libFiles, lib);
-child_process.execFileSync('tar', ['-xf', lib], {
-  cwd: __dirname,
-});
+if (platform === 'linux') {
+  unbr(libFiles, lib);
+  child_process.execFileSync('tar', ['-xf', lib], {
+    cwd: __dirname,
+  });
+}
 _cleanup();
-
-process.on('uncaughtException', err => {
-  console.warn(err.stack);
-});
